288dffdf9b20bfb3e21e98447d3f44ac8dd55dbe
PedroIgor <pedrode13souza@gmail.com>
2021-05-02 17:27:19 -0300

    Criando Dashboard

---
+++ lib/dashboard.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [IconButton(icon: Icon(Icons.chat), onPressed: () {})],
+      ),
+      body: Container(
+        //color: Colors.amberAccent,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black38,
+              spreadRadius: 0,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_circle, size: 28,),
+                label: 'add',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'favorite',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            onTap: _onItemTapped,
+            unselectedItemColor: Colors.black,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/dashboard.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Insta());
 }
 
-class MyApp extends StatelessWidget {
+class Insta extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Colors.grey,
         primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Instagram'),
+      home: Dashboard(title: 'Instagram'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            height: 50.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.home_filled),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.add_circle),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.favorite),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
-}
