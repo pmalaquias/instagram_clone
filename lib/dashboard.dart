e9c3f998b0015110a73e1d9139689b6634104990
PedroIgor <pedrode13souza@gmail.com>
2021-05-03 09:27:45 -0300

    creating files search and favorites

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -11,21 +11,6 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  int _selectedIndex = 0;
-
-  List _pages = [
-    Text("Home"),
-    Text("Order"),
-    Text("Notfication"),
-    Text("More"),
-    Text("Profile"),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,62 +24,8 @@ class _DashboardState extends State<Dashboard> {
           IconButton(icon: Icon(Icons.chat), onPressed: () {}),
         ],
       ),
-      body: Center(child: _pages[_selectedIndex]),
-      /*bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30),
-            topLeft: Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black38,
-              spreadRadius: 0,
-              blurRadius: 10,
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
-          ),
-          child: BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_filled),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.add_circle,
-                  size: 28,
-                ),
-                label: 'add',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'favorite',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
-              ),
-            ],
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.blue,
-            onTap: _onItemTapped,
-            unselectedItemColor: Colors.black,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-          ),
-        ),
-      ),*/
+      body: Center(),
+
     );
   }
 }
